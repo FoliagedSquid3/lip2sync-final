e42960990733eec01e7b82af6123b99e225321d7
alisidd-00 <malisiddiq0@gmail.com>
2024-07-23 12:56:18 +0500

    trying streaming pt 3

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -407,13 +407,12 @@ async def process_complete_job(background_tasks: BackgroundTasks,job_id: int, us
 #         raise HTTPException(status_code=500, detail="Failed to process video")
 
 @router.post("/send_video")
-async def send_video(request: Request):
+async def send_video(request: SendVideo):
     data = await request.json()
     print('data',data)
-    job_id = str(data['job_id'])
-    user_id = str(data['user_id']) + ".mp4"
-    print('user id',user_id)
-    video_path = os.path.join('result_dir', job_id, user_id)
+    job_id = str(request.job_id)
+    user_id = str(request.user_id) + ".mp4"
+    video_path = os.path.join(result_dir, job_id, user_id)
     print('video path',video_path)
     if not os.path.exists(video_path):
         raise HTTPException(status_code=404, detail="Video not found")
