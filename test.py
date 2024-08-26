cba0365ca63a9c434bd2946928793b4c6cd2cfc1
alisidd-00 <malisiddiq0@gmail.com>
2024-08-26 15:18:07 +0500

    project documentation added

--- test.py
+++
@@ -1,4 +0,0 @@
-from deep_translator import GoogleTranslator
-to_translate = 'I want to translate this text'
-translated = GoogleTranslator(source='auto', target='ru').translate(to_translate)
-print('translated', translated)
